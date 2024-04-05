a2d844d5414f5f4cdd768b7ac367c94dd532f1e8
sesychev <sergeysychou@gmail.com>
2024-04-05 11:15:00 +0300

    feat: getFirstResolvedPromiseResul

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -36,8 +36,11 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve, reject) => {
+    if (source) resolve('success');
+    else reject(new Error('fail'));
+  });
 }
 
 /**
@@ -53,8 +56,8 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises).then((value) => value);
 }
 
 /**
